597dd218eebb488624f622b958311c17248c79b1
tom <t.bass0326@gmail.com>
2017-01-03 02:33:51 +0900

    💻

--- keyboards/ergodox/keymaps/kus0tenpa/keymap.c
+++ keyboards/ergodox/keymaps/kus0tenpa/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
-        KC_LCTRL,     KC_A, KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        (KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,
+        KC_LCTRL,     KC_A,           KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,       KC_Z,           KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,
         KC_NO         ,KC_NO,      KC_BSLS,  KC_GRV, KC_LGUI,
                                                         KC_NO,  KC_NO,
                                                               KC_HOME,
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |RESET    |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+ * |         |   !  |   @  |   |  |   {  |   }  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |      |        |
+ * |         |   #  |   $  |   `  |   (  |   )  |------|           |------| Down |   4  |   5  |   6  |      |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
+ * |         |   %  |   ^  |   ~  |   [  |   ]  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | EPRM  |      |      |   +   |  =  |                                       |      |    . |   0  |   =  |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -83,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = KEYMAP(
        // left hand
        RESET,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+       KC_TRNS,KC_EXLM,KC_AT,  KC_PIPE, KC_LCBR,KC_RCBR,KC_TRNS,
+       KC_TRNS,KC_HASH,KC_DLR, KC_GRV, KC_LPRN,KC_RPRN,
+       KC_TRNS,KC_PERC,KC_CIRC,KC_TILD,KC_LBRC,KC_RBRC,KC_TRNS,
           EPRM,KC_TRNS,KC_TRNS,KC_PPLS,KC_EQL,
                                        RGB_MOD,KC_TRNS,
                                                KC_TRNS,
