1b0fb0f84188100accda59f40215ca86f435d53f
tom <yamamoto_tomoya@cyberagent.co.jp>
2017-01-19 13:40:36 +0900

    💻

--- keyboards/ergodox/keymaps/kus0tenpa/keymap.c
+++ keyboards/ergodox/keymaps/kus0tenpa/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_RBRC,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSPC,
-                          KC_H,   KC_J,  KC_K, LT(MDIA, KC_L), LT(SYMB, KC_SCLN), KC_ENT,
+                          KC_H,   KC_J,  KC_K,   KC_L, LT(SYMB, KC_SCLN), KC_ENT,
              KC_QUOT,     KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_DOWN,
                                   KC_LALT, KC_ESC, KC_NO,   KC_NO,      KC_NO,
              KC_NO,        CTL_T(KC_ESC),
