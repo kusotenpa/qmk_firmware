82568155fcbeea440179d8ae4091fcd4e0ab9b81
tom <t.bass0326@gmail.com>
2017-02-01 13:28:08 +0900

    💻

--- keyboards/ergodox/keymaps/kus0tenpa/keymap.c
+++ keyboards/ergodox/keymaps/kus0tenpa/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Tab  |           | '"   |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| Down   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |  \|  |   ~` | LGui |                                       | Alt  |  ESC |      |      |      |
+ *   |      |      |  \|  |   ~` | LGui |                                       | Alt  |  ESC |      |      |   +  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |Ctrl/Esc|
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RBRC,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSPC,
                           KC_H,   KC_J,  KC_K,   KC_L, LT(SYMB, KC_SCLN), KC_ENT,
              KC_QUOT,     KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_DOWN,
-                                  KC_LALT, KC_ESC, KC_NO,   KC_NO,      KC_NO,
+                                  KC_LALT, KC_ESC, KC_NO,   KC_NO,         KC_PPLS,
              KC_NO,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN, KC_LANG1, KC_SPC
