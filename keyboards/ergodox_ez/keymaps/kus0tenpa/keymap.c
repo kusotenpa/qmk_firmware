1e9427fd2599d4b618a3036e5eb90dfaca62ae0e
tom <t.bass0326@gmail.com>
2019-10-30 14:57:42 +0900

    update

--- keyboards/ergodox_ez/keymaps/kus0tenpa/keymap.c
+++ keyboards/ergodox_ez/keymaps/kus0tenpa/keymap.c
@@ -47,14 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_HOME,
                                                KC_SPC,KC_LANG2,KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_DEL,
-             KC_RBRC,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSPC,
-                          KC_H,   KC_J,  KC_K,   KC_L,  KC_SCLN,           KC_ENT,
-             KC_QUOT,     KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_DOWN,
-                                  KC_MINS, KC_ESC, KC_NO,   KC_NO,         KC_PPLS,
-             KC_UP, KC_DOWN,
-             KC_PGUP,
-             KC_PGDN, KC_LANG1, LT(SYMB, KC_SPC)
+        KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_DEL,
+        KC_RBRC,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSPC,
+                    KC_H,   LT(SYMB, KC_J),  KC_K,   KC_L,  KC_SCLN,           KC_ENT,
+        KC_QUOT,     KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_DOWN,
+                             KC_MINS, KC_ESC, KC_NO,   KC_NO,         KC_PPLS,
+        KC_UP, KC_DOWN,
+        KC_PGUP,
+        KC_PGDN, KC_LANG1, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
